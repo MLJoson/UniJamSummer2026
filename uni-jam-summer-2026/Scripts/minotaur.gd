a18ded92660fe2a8aa4b7e7089fdfff226cdbbe5
Oisin <Oisinkiely200@gmail.com>
2026-07-11 14:42:15 -0700

    Improved minotaur movement AI
    Restricted minotaur's FOV for player detection and improved the transition between states with a timer

--- uni-jam-summer-2026/Scripts/minotaur.gd
+++ uni-jam-summer-2026/Scripts/minotaur.gd
@@ -3,35 +3,50 @@ extends CharacterBody3D
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var player_detection = $PlayerDetection
 @onready var player = get_tree().get_first_node_in_group("Player")
+@onready var chase_transition_timer = $ChaseTransitionTimer
 
 @export_group("Movement")
 @export var acceleration: int = 30
 @export var defaultMaxSpeed: float = 2
 @export var friction: int = 40
 @export var chaseMaxSpeed: float = 3
+@export var wander_range: float = 10.0 # determines how far from the player the minotaur's target pos will be in wonder state
 
 var currentSpeed = defaultMaxSpeed
+var isTransitioningMovementStates: bool = false
 
-
-func _unhandled_input(event: InputEvent) -> void:
-	pass
+var state = WONDER
+# state machine
+enum {
+	CHASE,
+	WONDER,
+}
 
 func _physics_process(delta: float) -> void:
-	# changes move speed depending on if player is close
-	if player_detection.canSeePlayer == true:
-		currentSpeed = chaseMaxSpeed
-	else:
-		currentSpeed = defaultMaxSpeed
+	look_at(global_position + velocity) # makes minotaur look where it's walking
 	
+	if player: # set state to chase if player is visible
+		if player_detection.canSeePlayer == true:
+			state = CHASE
 	
-	if player: # get player position
-		navigation_agent.set_target_position(player.global_position)
+	match state:
+		CHASE:
+			currentSpeed = chaseMaxSpeed
+			navigation_agent.set_target_position(player.global_position)
+			movement(delta)
+		
+		WONDER:
+			currentSpeed = defaultMaxSpeed
+			if navigation_agent.is_navigation_finished():
+				set_wonder_position()
+			movement(delta)
 	
+
+func movement(delta):
 	var destination = navigation_agent.get_next_path_position()
 	
 	if navigation_agent.is_navigation_finished(): # If the minotaur is done, stop moving
 		velocity = velocity.move_toward(Vector3.ZERO, friction * delta)
-		move_and_slide()
 		return
 	else: # move towards next point
 		var local_destination = destination - global_position
@@ -40,5 +55,15 @@ func _physics_process(delta: float) -> void:
 		# Calculate velocity based on direction and speed.
 		velocity = velocity.move_toward(direction * currentSpeed, acceleration * delta)
 		
-		# Move and slide.
-		move_and_slide()
+	
+	move_and_slide()
+
+func set_wonder_position(): # choses a random point to walk towards near the player
+	var random_offset = Vector3(randf_range(-wander_range, wander_range), 0, randf_range(-wander_range, wander_range))
+	navigation_agent.set_target_position(player.global_position + random_offset)
+
+# ends chase state if player is out of sight for long enough
+func _on_player_detection_lost_sight_of_player() -> void:
+	chase_transition_timer.start()
+	await chase_transition_timer.timeout
+	state = WONDER
--- uni-jam-summer-2026/Scripts/player_detection.gd
+++ uni-jam-summer-2026/Scripts/player_detection.gd
@@ -4,22 +4,33 @@ extends Area3D
 @onready var collisionShape = $CollisionShape3D
 @onready var player = get_tree().get_first_node_in_group("Player")
 
+@export var fovDegrees: int = 100
+
 var isPlayerInRadius: bool = false
 var canSeePlayer: bool = false
+var fov = cos(deg_to_rad(fovDegrees))
+signal lostSightOfPlayer
 
 func _ready() -> void:
-	# set raycast length to same as detection area radius
+	# set raycast length to same as detection area radiuswwd
 	raycast.target_position = Vector3.FORWARD * collisionShape.shape.radius
 
 func _process(delta: float) -> void:
 	# point raycast towards player if within radius
 	if isPlayerInRadius == true:
-		raycast.look_at(player.global_position) 
 		
-		if raycast.get_collider() == player:
-			canSeePlayer = true
-		else:
-			canSeePlayer = false
+		var direction = global_position.direction_to(player.global_position)
+		var facing = global_transform.basis.tdotz(direction)
+		if facing > fov: # checks if player is within FOV
+			raycast.look_at(player.global_position) 
+			
+			if raycast.is_colliding():
+				if raycast.get_collider() == player:
+					canSeePlayer = true
+				else:
+					if canSeePlayer == true: # only emits once
+						lostSightOfPlayer.emit()
+					canSeePlayer = false
 	pass
 
 
@@ -31,3 +42,6 @@ func _on_body_entered(body: Node3D) -> void:
 func _on_body_exited(body: Node3D) -> void:
 	if body == player:
 		isPlayerInRadius = false
+		if canSeePlayer == true:
+			canSeePlayer = false
+			lostSightOfPlayer.emit()
