2573bbaabc3b2687ef0695468d86d9e511435714
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-07-12 13:27:59 -0700

    add temporary UI animation

--- uni-jam-summer-2026/Scenes/TestingBuilds/StartMenuManager.gd
+++ uni-jam-summer-2026/Scenes/TestingBuilds/StartMenuManager.gd
@@ -11,3 +11,11 @@ func _on_start_pressed() -> void:
 	#start_scene.instantiate()
 	#var sceneInstance = start_scene.instantiate()
 	get_tree().change_scene_to_packed(start_scene)
+
+
+func _on_start_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
