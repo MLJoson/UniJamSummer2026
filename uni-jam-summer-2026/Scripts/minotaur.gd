5b7928870bc2a9d8a3e0dd577d2036af3481fc25
MLJoson <littlemjoson@gmail.com>
2026-07-15 01:22:27 -0700

    Added minotaur charges.
    Charge wind up
    Charge until it hits something
    Stun for a couple of seconds

--- uni-jam-summer-2026/Scripts/minotaur.gd
+++ uni-jam-summer-2026/Scripts/minotaur.gd
@@ -4,13 +4,26 @@ extends CharacterBody3D
 @onready var player_detection = $PlayerDetection
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var chase_transition_timer = $ChaseTransitionTimer
+@onready var charge_timer = $ChargeWindupTimer
+@onready var stun_timer = $StunTimer
 
+#Movement variables
 @export_group("Movement")
 @export var acceleration: int = 30
 @export var defaultMaxSpeed: float = 2
 @export var friction: int = 40
 @export var chaseMaxSpeed: float = 3
 @export var wander_range: float = 10.0 # determines how far from the player the minotaur's target pos will be in wonder state
+
+#Charge variables
+@export_group("Charge")
+@export var charge_speed := 10.0
+@export var charge_distance := 20.0
+@export var charge_windup := 1.0
+@export var stun_time := 2.0
+
+var charge_destination : Vector3
+var charge_direction : Vector3
 
 var currentSpeed = defaultMaxSpeed
 var isTransitioningMovementStates: bool = false
@@ -20,6 +33,9 @@ var state = WONDER
 enum {
 	CHASE,
 	WONDER,
+	CHARGE_WINDUP,
+	CHARGE,
+	STUN,
 }
 
 func _physics_process(delta: float) -> void:
@@ -27,7 +43,13 @@ func _physics_process(delta: float) -> void:
 	
 	if player: # set state to chase if player is visible
 		if player_detection.canSeePlayer == true:
-			state = CHASE
+			if player_detection.canSeePlayer:
+				if player_detection.canSeePlayer:
+					if state == WONDER:
+						if global_position.distance_to(player.global_position) > 8:
+							start_charge()
+						else:
+							state = CHASE
 	
 	match state:
 		CHASE:
@@ -40,7 +62,25 @@ func _physics_process(delta: float) -> void:
 			if navigation_agent.is_navigation_finished():
 				set_wonder_position()
 			movement(delta)
-	
+			
+		CHARGE_WINDUP:
+			velocity = Vector3.ZERO
+		CHARGE:
+			velocity = charge_direction * charge_speed
+			move_and_slide()
+			if global_position.distance_to(charge_destination) < 0.5:
+				state = STUN
+				stun_timer.start(stun_time)
+				
+			for i in range(get_slide_collision_count()):
+				var collision = get_slide_collision(i)
+				if collision:
+					state = STUN
+					stun_timer.start(stun_time)
+					velocity = Vector3.ZERO
+					break
+		STUN:
+			velocity = Vector3.ZERO
 
 func movement(delta):
 	var destination = navigation_agent.get_next_path_position()
@@ -67,3 +107,28 @@ func _on_player_detection_lost_sight_of_player() -> void:
 	chase_transition_timer.start()
 	await chase_transition_timer.timeout
 	state = WONDER
+
+#starts charge
+func start_charge():
+	state = CHARGE_WINDUP
+	velocity = Vector3.ZERO
+	charge_direction = -transform.basis.z.normalized()
+	var space = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, global_position + charge_direction * charge_distance)
+	query.exclude = [self]
+	var result = space.intersect_ray(query)
+	if result:
+		charge_destination = result.position
+	else:
+		charge_destination = global_position + charge_direction * charge_distance
+	charge_timer.start(charge_windup)
+
+func _on_charge_windup_timer_timeout() -> void:
+	state = CHARGE
+
+
+func _on_stun_timer_timeout() -> void:
+	if player_detection.canSeePlayer:
+		state = CHASE
+	else:
+		state = WONDER
